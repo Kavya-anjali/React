0a31ebf557ce33cb912ded918c945d25ed080cee
Kavya anjali <103109906+Kavya-anjali@users.noreply.github.com>
2023-03-01 17:22:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App=()=>{
+    return(
+        <>
+        <div> hello </div>
+        <p>hey</p>
+        <p>(2+3)</p>
+        <div>bye</div>
+        </>
+    )
+}
+export default App
