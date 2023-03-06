6270ad47e8a1cf73cd5a9b0a43ce494ea65df22f
Kavya anjali <103109906+Kavya-anjali@users.noreply.github.com>
2023-03-06 14:21:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Head from "./components/Head";
+import Logo1 from "./components/Logo1";
+import Logo2 from "./components/Logo2";
+
+
+const App=()=>{
+    return(
+        <>
+        <div> 
+            
+             <Footer/>
+             <Head/>
+             <Logo1/>
+             <Logo2/>
+        
+        </div>
+      
+       </>
+       
+    )
+}
+export default App;
