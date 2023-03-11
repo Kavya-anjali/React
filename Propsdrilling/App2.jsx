f3698d226d663f1165946d9d021a14cf6f949045
Kavya anjali <103109906+Kavya-anjali@users.noreply.github.com>
2023-03-11 16:30:37 +0530

    Add files via upload

---
+++ Propsdrilling/App2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Course from './Course';
+
+const App2 = () => {
+  return (
+   < Course  courseDetails={{
+    courseName:"Java FullStack",
+    courseAvail:true,
+    courseDuration:"2 months"
+
+   }}/>
+  )
+}
+
+export default App2;
